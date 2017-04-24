b9cccbf82fbb6e5392da6aca189b867585e6b9a6
andre.bonfatti <andrebnf@gmail.com>
2017-04-24 17:53:23 -0300

    added example for lps

---
+++ 1-intro/pages-json-example/json1.js
@@ -0,0 +1,5 @@
+window.json1 = {
+  title: 'LP1',
+  content: 'Lorem ipsum',
+  imageURL: 'http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg'
+};
---
+++ 1-intro/pages-json-example/json2.js
@@ -0,0 +1,4 @@
+window.json2 = {
+  title: 'LP2',
+  content: 'Lorem ipsum 2'
+};
